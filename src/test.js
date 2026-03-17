914527110b89f349ed2166f53cfc70b05455c4aa
ruturajdadas4831 <ruturajdadas519@gmail.com>
2026-03-18 03:27:41 +0530

    added test file for AI review

---
+++ src/test.js
@@ -0,0 +1,18 @@
+function login(user, password) {
+    var adminPassword = "12345"
+
+    if(user == "admin" && password == adminPassword){
+        console.log("Login success")
+        return true
+    } else {
+        console.log("Login failed")
+    }
+}
+
+let x = 10
+
+function calculate(a, b){
+    return a + b
+}
+
+
